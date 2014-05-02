606e3f05fcc7af0377c9de0916a1aab0ef5b9455
Jens Jahnke <jan0sch@gmx.net>
2014-05-02 17:19:49 +0200

    CodecJson examples were defined with DecodeJson
    The defintions of the CodecJson examples were defined
    using `DecodeJson[Person]` instead of `CodecJson[Person]` this was
    somewhat misleading. At least for me. ;-)

--- src/main/scala/argonaut/doc/CodecJsonDefaultExample.scala
+++ src/main/scala/argonaut/doc/CodecJsonDefaultExample.scala
@@ -8,13 +8,13 @@ object CodecJsonDefaultExample extends App {
 
   // Default CodecJson instance for case classes.
 
-  implicit def PersonCodecJson: DecodeJson[Person] =
+  implicit def PersonCodecJson: CodecJson[Person] =
     casecodec2(Person.apply, Person.unapply)("name", "age")
 
   // Note that casecodec2 makes assumptions about the unapply
   // method (namely that it will always return Some), for a
   // an exmplic version of this see:
 
-  def ExplicitPersonCodecJson: DecodeJson[Person] =
+  def ExplicitPersonCodecJson: CodecJson[Person] =
     codec2(Person.apply, (Person.unapply _) andThen (_.get))("name", "age")
 }
